825bfa4dacfb808e6f600df3314182f6e57cbbc3
Nory Lazo <nlazo@icloud.com>
2019-11-13 15:48:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,10 +13,25 @@ def key_for_min_value(name_hash)
   # end
   # lowest_key
   
-  if name_hash = {}
+  # if name_hash = {}
+  #   nil
+  # else 
+  #   lowest_value = name_hash.first[0]
+  #   lowest_key = name_hash.first[0]
+  # end
+  
+    if name_hash == {}
     nil
-  else 
-    lowest_value = name_hash.first[0]
+  else
+  # set the lowest key and value to the first key value pair
     lowest_key = name_hash.first[0]
-  end 
+    lowest_value = name_hash.first[1]
+    name_hash.each do |key, value|
+      if value < lowest_value
+        lowest_value = value
+        lowest_key = key 
+      end
+    end
+    lowest_key
+  end
 end
